63f3fcbdc9c4f8116cb8350ea0f70877c9f2a7ea
edmondchui <chui.hinwai@gmail.com>
2019-02-18 13:35:46 -0700

    *minor refactor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 import {DropzoneArea} from "material-ui-dropzone";
 
-class App extends Component {
-  handleChange(files) {
-    this.setState({
-      files: files
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
-        <DropzoneArea
-          onChange={this.handleChange.bind(this)}
-        />
-      </div>
-    );
-  }
+export function App() {
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
+      <DropzoneArea/>
+    </div>
+  );
 }
-
-export default App;

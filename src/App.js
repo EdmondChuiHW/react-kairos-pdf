e33687fb94f80be6fa4d64a79dfc8aad453e5d3d
edmondchui <chui.hinwai@gmail.com>
2019-02-18 16:14:11 -0700

    *update locales

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import {DropzoneArea} from "material-ui-dropzone";
+import {useTranslation} from "react-i18next";
+import CssBaseline from "@material-ui/core/es/CssBaseline";
 
 export function App() {
+  const {t, i18n} = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
     <div className="App">
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"/>
+      <CssBaseline/>
+      <h1>{t('title')}</h1>
+      <button onClick={() => changeLanguage('en')}>en</button>
+      <button onClick={() => changeLanguage('zh-hk')}>zh-hk</button>
+      <button onClick={() => changeLanguage('zh-cn')}>zh-cn</button>
       <DropzoneArea/>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './i18n';
 import {App} from './App';
 import * as serviceWorker from './serviceWorker';
+import CircularProgress from "@material-ui/core/es/CircularProgress";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+  <Suspense fallback={<CircularProgress/>}>
+    <App/>
+  </Suspense>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

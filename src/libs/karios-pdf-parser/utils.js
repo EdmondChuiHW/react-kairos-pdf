e31d81d998d7613cc2c72c0730ca0176b706f7ba
edmondchui <chui.hinwai@gmail.com>
2019-03-24 16:27:05 -0600

    +add support for ft

--- src/libs/karios-pdf-parser/utils.js
+++ src/libs/karios-pdf-parser/utils.js
@@ -1,6 +1,6 @@
 import {either, isEmpty, isNil, test} from "ramda";
 
-export const startsWithGpgOrFt = test(/^(?:ft)|^(?:gpg)/i);
+export const startsWithGpgOrFt = test(/^(?:ft)|^(?:facilitator)|^(?:gpg)/i);
 export const isNilOrEmpty = either(isNil, isEmpty);
 export const stripAllQuotes = s => s.replace(/['"]+/g, '');
 
